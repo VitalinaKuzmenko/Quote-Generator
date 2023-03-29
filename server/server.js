e8402c0d5641ce201fc9df4e502403c27c83f4d4
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-03-29 21:13:01 +0100

    Adding server.js file and fetching data from api from server

---
+++ server/server.js
@@ -0,0 +1,43 @@
+const lodash = require("lodash");
+const express = require("express");
+const app = express();
+const port = 3030;
+
+const cors = require("cors");
+app.use(cors());
+
+//start listening to server
+app.listen(port, function () {
+  console.log("Your app is listening on port ");
+});
+
+//load the quotes JSON
+const quotes = require("./src/quotes.json");
+
+// http://localhost:3030/ - Return some helpful welcome info (text)
+app.get("/", function (request, response) {
+  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+});
+
+//   http://localhost:3030/quotes            - Should return all quotes (json)
+app.get("/quotes", function (request, response) {
+  response.send(quotes);
+});
+
+//   http://localhost:3030/quotes/random    - Should return ONE quote (json)
+app.get("/quotes/random", function (request, response) {
+  let randomQuote = lodash.sample(quotes);
+  response.send(randomQuote);
+});
+
+//   http://localhost:3030/quotes/search?term=life
+app.get("/quotes/search", (request, response) => {
+  const searchTerm = request.query.term.toLowerCase(); // Get the search term from the query parameter and convert to lowercase
+  console.log(searchTerm);
+  const filteredQuotes = quotes.filter(
+    (quote) =>
+      quote.quote.toLowerCase().includes(searchTerm) ||
+      quote.author.toLowerCase().includes(searchTerm)
+  ); // Filter the quotes array to include only those that contain the search term in the text
+  response.json(filteredQuotes); // Return the filtered quotes as JSON
+});
--- src/QuoteButton.js
+++ src/QuoteButton.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 
 export default function QuoteButton({ setQuote }) {
-  const [fake, setFake] = useState("");
+  // const [fake, setFake] = useState("");
 
   const getAnotherQuote = () => {
-    fetch("https://morning-glacier-wildflower.glitch.me/quotes/random")
+    fetch("http://localhost:3030/quotes/random")
       .then((response) => response.json())
-      .then((data) => setFake(data))
+      .then((data) => setQuote(data))
       .catch((error) => console.log(error));
 
-    console.log(fake);
+    // console.log(fake);
   };
 
   return (
